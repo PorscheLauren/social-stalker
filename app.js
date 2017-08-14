7bb314baf59d9ff0d6f07e9e2f43f7f1db94f0d5
Seregy <seregy77@gmail.com>
2017-08-14 21:39:38 +0300

    Add and update routers, add hbs helper
    - Add routers for help and settings pages and update index router
    - Add hbs helper for adding 'active' class for usage in navbar
    - Register hbs partials folder
    - Add paths to plugins' resources

--- app.js
+++ app.js
@@ -3,24 +3,42 @@ const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const hbs = require('hbs');
 
 const index = require('./routes/index');
-const users = require('./routes/users');
+const settings = require('./routes/settings');
+const help = require('./routes/help');
 
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
+hbs.registerHelper('navActive', function(activePage) {
+  if (activePage === this.page) {
+    return 'active';
+  }
+});
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/scripts', express.static(
+  path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'js')));
+app.use('/stylesheets', express.static(
+  path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css')));
+app.use('/scripts', express.static(
+  path.join(__dirname, 'node_modules', 'jquery', 'dist')));
+app.use('/scripts', express.static(
+  path.join(__dirname, 'node_modules', 'popper.js', 'dist')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/index', index);
+app.use('/settings', settings);
+app.use('/help', help);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
