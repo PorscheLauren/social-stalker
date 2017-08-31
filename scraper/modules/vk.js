592b8ca512e18d099ed080bc21586fb50853cb41
Seregy <seregy77@gmail.com>
2017-09-01 01:06:17 +0300

    Turn VK scraper into class, add new methods

--- scraper/modules/vk.js
+++ scraper/modules/vk.js
@@ -1,40 +1,101 @@
 'use strict';
 
 const VKApi = require('node-vkapi');
-const VK = new VKApi({
-  app: {
-    id: 5859550,
-    secret: 'MyrpeST0YMnvJoQBQyYT',
-  },
-  token: 'b83415a55436eaa53a6ca06de49c34e5471877e04f025883' +
-   '8775d46835e0becb0e931e3df5aec649c04dd',
-});
 
 /**
- * 
- * @param {*} callback 
+ * Class representing scraper from social network VK.
  */
-function fetchUsers(callback) {
-    VK.call(
-        'friends.get',
-         {
-           fields: 'last_seen',
-       }).then((res) => {
-           let users = [];
-           res.items.forEach(function(element) {
-               users.push(
-                   {id: element.id,
-                    first_name: element.first_name,
-                    last_name: element.last_name,
-                    online: element.online == 1 ? true : false,
-                    last_seen:
-                        element.last_seen ? element.last_seen.time : undefined,
-                    source: 'vk'});
-           });
-           callback(users);
-        }).catch((error) => {
-            console.log('[' + new Date() + '] '+ error);
+class VK {
+    /**
+     * Create VK scraper.
+     *
+     * @param {object} options scraper's options, can contain appId,
+     * appSecureKey and userToken.
+     */
+    constructor(options = {}) {
+        this._appId = options.appId ? options.appId : '';
+        this._appSecureKey = options.appSecureKey ? options.appSecureKey : '';
+        this._userToken = options.userToken ? options.userToken : '';
+
+        this._vkApi = new VKApi({
+            app: {
+                id: this._appId,
+                secret: this._appSecureKey,
+            },
+            token: this._userToken,
         });
+    }
+
+    /**
+     * Return name of the scraper.
+     */
+    static get NAME() {
+        return 'vk';
+    }
+
+    /**
+     * Set scraper's options.
+     *
+     * @param {object} options scraper's options, can contain appId,
+     * appSecureKey and userToken.
+     */
+    setOptions(options = {}) {
+        if (options.appId) {
+            this._appId = options.appId;
+        }
+        if (options.appSecureKey) {
+            this._appSecureKey = options.appSecureKey;
+        }
+        if (options.userToken) {
+            this._userToken = options.userToken;
+        }
+
+        this._vkApi.setOptions({
+            app: {
+                id: this._appId,
+                secret: this._appSecureKey,
+            },
+            token: this._userToken,
+        });
+    }
+
+    /**
+     * Callback format for results
+     * @callback VK~resultCallback
+     * @param {object} error error instance representing
+     * the error during the execution
+     * @param {object} result result object if the command
+     * was executed successfully
+     */
+
+    /**
+     * Fetch users from VK.
+     *
+     * @param {VK~resultCallback} callback result callback
+     */
+    fetchUsers(callback) {
+        this._vkApi.call(
+            'friends.get',
+             {
+               fields: 'last_seen',
+           }).then((res) => {
+                let users = [];
+                res.items.forEach(function(element) {
+                    users.push(
+                        {id: element.id,
+                        first_name: element.first_name,
+                        last_name: element.last_name,
+                        online: element.online == 1 ? true : false,
+                        last_seen:
+                         element.last_seen ? element.last_seen.time : undefined,
+                        source: VK.NAME});
+                });
+                callback(null, users);
+            })
+            .catch(function(err) {
+                callback(err);
+            });
+    }
 }
 
-exports.fetchUsers = fetchUsers;
+module.exports = VK;
